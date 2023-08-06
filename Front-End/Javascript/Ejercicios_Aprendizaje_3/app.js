12e3596e58a7f023ba1f6d294b4c906d1d07fa2b
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2023-08-06 18:02:01 -0300

    update app.js

--- Front-End/Javascript/Ejercicios_Aprendizaje_3/app.js
+++ Front-End/Javascript/Ejercicios_Aprendizaje_3/app.js
@@ -1,9 +1,9 @@
 function verificarEdad() {
     // Obtener el valor ingresado por el usuario
-    var edadUsuario = document.getElementById('edad').value;
+    let edadUsuario = document.getElementById('edad').value;
 
     // Convertir el valor a número entero
-    var edad = parseInt(edadUsuario);
+    let edad = parseInt(edadUsuario);
 
     // Verificar si el usuario es mayor de edad (mayor o igual a 18 años)
     if (edad >= 18) {
