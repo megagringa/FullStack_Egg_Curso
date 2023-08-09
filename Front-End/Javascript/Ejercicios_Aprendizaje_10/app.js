f3af96882b3d2f742780784a6d587b522abfebec
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2023-08-08 23:50:59 -0300

    Update.app();

--- Front-End/Javascript/Ejercicios_Aprendizaje_10/app.js
+++ Front-End/Javascript/Ejercicios_Aprendizaje_10/app.js
@@ -1,6 +1,10 @@
 // Definir la función flecha que invierte una palabra
+//.split =>convierte la cadena ingresa en un array de sub cadenas . ('') -> probar con - dentro de las comillas
+//.reverse => invierte el orden de sub cadenas.
+//.join => une elementos de una cadena de texto. ('') -> probar con - dentro de las comillas 
 const reverseWord = (word) => {
     return word.split('').reverse().join('');
+    //return word.reverse(); -> asi no muestra nada.
   };
   
   // Evento al hacer clic en el botón
